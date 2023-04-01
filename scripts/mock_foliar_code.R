702a0a71ad8f2ca57ddc685a7a158878ceab1fac
Hannah Houston <houstonh@g.cofc.edu>
2023-04-01 16:55:08 -0400

    Editing mock foliar code to reflect smaller-batch Anova's instead of categorical dummy variables

--- scripts/mock_foliar_code.R
+++ scripts/mock_foliar_code.R
@@ -2,55 +2,62 @@
 mock_foliar <- read.csv(file = './data/mock_foliar.csv')
 head(mock_foliar)
 
-#make a graph (use this code later, just keep in here in case you need it)
+#####Power Analysis############################################################################# 
+#####Conduct a power analysis to determine if there are enough experimental replications to detect a trend
+
+
+
+
+#make a graph looking at all treatment types and export as graph to figs folder
 library(ggplot2)
-par(mfrow=c(2,1))
-ggplot(bold, aes(x=Type,y=PC1,fill=Type))+geom_boxplot()+ylab("Atomic Percent Enrichment (APE)")+xlab("Snail Type")
-ggplot(bold, aes(x=Type,y=PC2,fill=Type))+geom_boxplot()+ylab("Atomic Percent Enrichment (APE)")+xlab("Snail Type")
+pdf('./figs/mock_foliar_all_treatments.pdf') #pdf will show up in figs folder
+par(mfrow = c(1,1))
+ggplot(mock_foliar, aes(x=treatment,y=APE,fill=treatment))+geom_boxplot()+ylab("Atomic Percent Enrichment (APE)")+xlab("Treatment")
+dev.off() #where to stop pdf
 
 
-#Categorical predictors in regression: values of the categorical predictors are usually referred to as treatments/treatment levels
-#my categories are the treatment levels:
-    #no_defol, con_no_defol, 0.5_donor_defol, 0.5_recip_defol, con_0.5_donor_defol, con_0.5_recip_defol, 0.75_donor_defol, 0.75_recip_defol, con_0.75_donor_defol, con_0.75_recip_defol, 1.0_donor_defol, 1.0_recip_defol, con_1.0_donor_defol, con_1.0_recip_defol
-#each treatment has different tissue groups:
-    #needles, stem, lowroot, highroot
-#to include in the regression, create a set of dummy (indicator) variables. Once created, those dummy variables are called regressors
-#create n-1 dummy variables (so for tissues, create 3 and for treatment levels make 13)
 
 
-#Create dummy variables using tissues as categorical predictor
-#load the package
-library('fastDummies')
 
-#Create dummy variables for donor or recipient seedling:
-dummy_don_recip <- dummy_cols(mock_foliar, select_columns = 'donor_or_recip')
-dummy_don_recip
+#####No Defol############################################################################# 
+#####No defoliaiton occured in this experimental treatment. 
+#####In the control group, cores were rotated to sever mycorrhizal connections.
 
-#Create dummy variables for tissue type:
-dummy_tissue <- dummy_cols(mock_foliar, select_columns = 'tissue')
-dummy_tissue
+#subset the data
+no_defol <- subset(mock_foliar, treatment %in% c('no_defol', 'con_no_defol'))
+View(no_defol)
+data.frame(no_defol)
 
-#create dummy variables using treatment as categorical predictor
-#load the package
-library('fastDummies')
-# Create dummy variables for treatment:
-dummy_treat <- dummy_cols(mock_foliar, select_columns = 'treatment')
-dummy_treat
+#make a graph: 
+pdf('./figs/no_defol.vs.con_no_defol.pdf') #pdf will show up in figs folder
+par(mfrow = c(1,1))
+ggplot(no_defol, aes(x= treatment, y=APE,fill=treatment))+geom_boxplot()+ylab("Atomic Percent Enrichment (APE)")+xlab("Treatment")
+dev.off()
 
-#or was I supposed to make dummy variables of two columns? 
-dummy_mock_foliar <- dummy_cols(mock_foliar, select_columns = c('treatment', 'tissue'))
-dummy_mock_foliar
-
-#do I need to append the APE values to this?
-mock_foliar_dummy <- cbind(dummy_tissue, dummy_treat, dummy_don_recip, mock_foliar$APE)
-mock_foliar_dummy
-
-
-#create the model
+#Create a model: no defol vs. control no defol
 library(car)
-lm_mock_foliar <- lm(APE ~ dummy_tissue + dummy_treat + dummy_don_recip, data=mock_foliar_dummy)
-Anova(lm_mock_foliar)
-summary(lm_mock_foliar)
+lm_no_defol <- lm(APE ~ treatment, data=no_defol)
+Anova(lm_no_defol)
+summary(lm_no_defol)
+
+#check residuals of the model
+par(mfrow = c(2, 2))
+plot(lm_no_defol) 
+plot(lm_no_defol) 
+#the residuals vs. fitted and the Scale-Location graphs: look for normal spread of points along red line, patterns show issues with homoscedasticity
+#The Q-Q plot: look for points hugging red line, deviance from line shows issues with residuals
+#The Residuals vs Leverage graph indicates if there are large outliers that have a disproportionate effect on the regression
+
+#Tukey test to see if different plant tissue groups enriched differently
+#subset the data more to look at tissue groups and APE for no_defol treatment
+tissue_no_defol <- subset(mock_foliar, treatment %in% c('no_defol'))
+View(tissue_no_defol)
+data.frame(tissue_no_defol)
+#build the model and run TukeyHSD
+aov_tissue_no_defol <- aov(APE ~ tissue, data = tissue_no_defol)
+summary(aov_tissue_no_defol)
+TukeyHSD(aov_tissue_no_defol, 'tissue', ordered = TRUE, conf.level = 0.95)
+plot(TukeyHSD(aov_no_defol, 'tissue'))
 
 
-#look at power test, pull error values from data
+
