73b9cdbc6c8234400f3156832ae4f5f4f691319b
Hannah Houston <houstonh@g.cofc.edu>
2023-04-05 14:53:40 -0400

    adding metadata to the data folder

---
+++ data/metadata.R
@@ -0,0 +1,35 @@
+#metadata for mock_foliar, mock_free_planting, and mock_stem
+
+#plant_num: the individual number assigned to each seedling
+  
+#isotope_label: the way in which isotopic labels were applied to the seedlings; foliar or stem.
+  #foliar application: an aqueous solution of 13C and 15N2 was painted onto the needles of seedlings
+  #stem infiltration labeling: a hole was drilled into the stems of seedlings and a cotton wick was run through the hole. The wick was placed in a reservoir containing a 13C and 15N2 solution 
+  
+#treatment: refers to the level of defoliation applied to the seedling to trigger a root death event
+  #no_defol: seedlings were not defoliated 
+  #con_no_defol: seedlings were not defoliated, cores were rotated to sever mycorrhizal connections
+  #0.5_donor_defol: 50% of the needles were removed from donor seedlings
+  #0.5_recip_defol: 50% of the needles were removed from recipient seedlings
+  #con_0.5_donor_defol: 50% of the needles were removed from donor seedlings, cores were rotated to sever mycorrhizal connections
+  #con_0.5_recip_defol: 50% of the needles were removed from recipient seedlings, cores were rotated to sever mycorrhizal connections
+  #0.75_donor_defol: 75% of the needles were removed from donor seedlings
+  #0.75_recip_defol: 75% of the needles were removed from recipient seedlings
+  #con_0.75_donor_defol:75% of the needles were removed from donor seedlings, cores were rotated to sever mycorrhizal connections
+  #con_0.75_recip_defol: 75% of the needles were removed from recipient seedlings, cores were rotated to sever mycorrhizal connections
+  #1.0_donor_defol: 100% of the needles were removed from donor seedlings
+  #1.0_recip_defol: 100% of the needles were removed from recipient seedlings
+  #con_1.0_donor_defol: 100% of the needles were removed from donor seedlings, cores were rotated to sever mycorrhizal connections
+  #con_1.0_recip_defol: 100% of the needles were removed from recipient seedlings, cores were rotated to sever mycorrhizal connections
+
+#donor_or_recipient: donor seedlings were isotopically labeled, recipient seedlings were not isotopically labeled
+  #d: donor seedlings
+  #r: recipient seedings
+  
+#tissue: when harvested, seedlings tissues were separated into four different tissue groups for analysis
+  #needles: the needles from the seedling
+  #stem: the stem of the seedling
+  #lowroot: 1st - 3rd order roots from the seedlings
+  #highroot: 4th - highest order roots from the seedlings
+  
+#APE: Atomic Percent Enrichment of samples (15N2 and 13C) from IRMS analysis of plant tissues
