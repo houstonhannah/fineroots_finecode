cdda69bf48b6a1887c5b116fd6ecf0c6dd9513a9
Hannah Houston <houstonh@g.cofc.edu>
2023-04-05 15:31:57 -0400

    editing tukeyHSD tests for control and experimental groups

--- scripts/mock_foliar_code.R
+++ scripts/mock_foliar_code.R
@@ -17,6 +17,7 @@ library(pwr)
 pwr.anova.test(f= , k=26, n=5, sig.level=0.05)
 
 #f is the effect size, no data exists yet in the literature about possible effect size. 
+
 #f will be calculated using preliminary data that is currently being analyzed.
 #k is the number of groups
 #n is the number of pots per group
@@ -235,18 +236,42 @@ lm_defol_0.5_con_ronly <- lm(APE ~ treatment, data=defol_0.5_con_ronly)
 Anova(lm_defol_0.5_con_ronly)
 summary(lm_defol_0.5_con_ronly)
 
+#####do Tukey Tests for all tissue groups
+##Experimental Group Tukey Tests
+
+#subset the data more to look at tissue groups and APE for no_defol treatment
+tissue_con_no_defol <- subset(mock_foliar, treatment %in% c('con_no_defol'))
+View(tissue_con_no_defol)
+data.frame(tissue_con_no_defol)
+
+#build the model and run TukeyHSD
+aov_tissue_con_no_defol <- aov(APE ~ tissue, data = tissue_con_no_defol)
+summary(aov_tissue_con_no_defol)
+tukey <- TukeyHSD(aov_tissue_con_no_defol, 'tissue', ordered = TRUE, conf.level = 0.95)
+#compact letter display
+cld <- multcompLetters4(aov_tissue_con_no_defol, tukey)
+
+#table with factors and 3rd quartile 
+tissue_waje <- group_by(tissue_con_no_defol, tissue) %>%
+  summarise(w=mean(APE),sd = sd(APE)) %>%
+  arrange(desc(w))
+
+#extracting compact letter display and adding to the Tukey table
+cld <- as.data.frame.list(cld$tissue)
+tissue_waje$cld <- cld$Letters
+print(tissue_waje) #this prints the letters that are assigned based on sigificance, note that for the "mock" data these will not be significant
+
+#build the graph and save as a PDF to figs folder 
+pdf('./figs/tukey_control_nodefol_tissue.pdf') #pdf will show up in figs folder
+ggplot(tissue_waje, aes(tissue, w)) + 
+  geom_bar(stat = "identity", aes(fill = tissue), show.legend = TRUE) +
+  geom_errorbar(aes(ymin = w-sd, ymax=w+sd), width = 0.05) +
+  labs(x = "Plant Tissue Types", y = "Atomic Percent Enrichment") +
+  geom_text(aes(label = cld, y = w + sd), vjust = -0.5)
+dev.off() #where to stop pdf
+
+##Control Group Tukey Tests
 
 
-#####0.75 Defol#############################################################################
-#####In this experimental treatment, designated seedlings had 75% of their needles removed to trigger a root senescence event 
-#####In the control group, cores were rotated to sever mycorrhizal connections, preventing isotopes from moving via mycorrhizae
-
-#this will be populated with a duplicate of the above code
-
-
-
-#####1.0 Defol#############################################################################
-#####In this experimental treatment, designated seedlings had 100% of their needles removed to trigger a root senescence event 
-#####In the control group, cores were rotated to sever mycorrhizal connections, preventing isotopes from moving via mycorrhizae
-
-#this will be populated with a duplicate of the above code
+#####0.75 Defol and 1.0 Defol#############################################################################
+#in order to analyze APE for the 0.75 defol and 1.0 defol, the above code will be replicated.
